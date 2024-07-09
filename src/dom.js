0fbc4f0a95a0196d6ee20adcf4e4b2e227783a15
minjupgeorge <minjupgeorge>
2024-07-09 13:48:59 +0200

    files added

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+export const dom = {
+    resetBtn: document.querySelector("#resetBtn"),
+    title: document.querySelectorAll('.tile'),
+    playerDisplay: document.querySelector('.display-player'),
+    announcer: document.querySelector('.announcer')
+}
